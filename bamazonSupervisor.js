42883872c2435caf8e225f2139408fa81ed981c7
Dialest <dialest@hotmail.com>
2019-03-22 20:08:17 -0700

    Completed Supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,9 +1,118 @@
-// // Add overhead costs to product table for each item
-// // add product sales column to products table
-// // add price to product sales in customer app
-// // Add overhead cost to manager add inventory
-// // Add overhead cost to manager for add item
-// make menu fro supervisor
-// departmentID    departmentName  costs   sales   profit
-// Make table
-// make add department
+require("dotenv").config();
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) { throw err };
+    console.log("Connected as ID " + connection.threadId);
+    menu();
+});
+
+function menu () {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Welcome, supervisor!\nWhat would you like to do today?",
+            choices: ["View Product Sales by Department", "Create New Department"],
+            name: "menu"
+        }
+    ]).then(function (answer) {
+        console.log();
+        execute(answer.menu);
+    });
+};
+
+
+function execute(choice) {
+    switch (choice) {
+        case "View Product Sales by Department":
+            departments();
+            break;
+        case "Create New Department":
+            newDepartment();
+            break;
+    };
+};
+
+function departments() {
+    connection.query(`SELECT departments.id AS ID, departments.name AS Department, SUM(products.overhead_cost) AS Overhead, SUM(products.gross) AS "Gross", SUM(products.gross) - SUM(products.overhead_cost) AS Profits
+    FROM departments LEFT JOIN products ON products.department = departments.id
+    GROUP BY departments.id;`, function (err, res) {
+        if (err) {throw err };
+        // console.log(res);
+        console.log("ID\t" + "Department".padEnd(20, " ") + "Overhead".padEnd(20, " ") + " Gross Sales".padEnd(20, " ") + "\t" + "Profits".padEnd(20, " "));        
+        for (i=0; i<res.length; i++) {
+            var overhead = res[i].Overhead;
+            if (overhead === null) {overhead = 0};
+            var gross = res[i].Gross;
+            if (gross === null) {gross = 0};
+            var profits = res[i].Profits;
+            if (profits === null) {profits = 0};
+            console.log(res[i].ID + "\t" + res[i].Department.padEnd(20, " ") + "$" + overhead.toFixed(2).padEnd(20, " ")  + "$" + gross.toFixed(2).padEnd(20, " ") + "\t$" + profits.toFixed(2).padEnd(20, " "));
+        };
+        console.log();
+        another();
+    })
+}
+
+function newDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter name of department:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter department description:",
+            name: "description"
+        },
+        {
+            type: "confirm",
+            message: "Are you sure:",
+            name: "confirm",
+            default: true
+        }
+    ]).then(function(answers) {
+        if (answers.confirm) {
+            connection.query(`INSERT INTO departments (name, description) 
+            VALUES ("${answers.name}", "${answers.description}")`, function (err, res) {
+                if (err) { throw err };
+                console.log(res.affectedRows + " record(s) updated");
+                another();
+            })
+        }
+        else {
+            console.log("Action cancelled.");
+            another();
+        };
+    });
+
+};
+
+function another() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Do you wish to perform another task?",
+            name: "another",
+            default: true
+        }
+    ]).then(function (answer) {
+        if (answer.another) {
+            menu();
+        }
+        else {
+            console.log("Have a great day!");
+            connection.end();
+        }
+    })
+};
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
 USE bamazon;
@@ -24,7 +25,9 @@ CREATE TABLE products (
     product_name		VARCHAR(50) NULL,
     product_description	VARCHAR(250) NULL,
     customer_price		DECIMAL(10,2) NULL,
+    overhead_cost		DECIMAL(10,2) NULL,
     stock_quantity		INT DEFAULT 0,
+    gross				DECIMAL(10,2) DEFAULT 0,
     department			INT NOT NULL,
     PRIMARY KEY (item_id),
     FOREIGN KEY fk_department (department)
@@ -32,20 +35,20 @@ CREATE TABLE products (
 );
 
 USE bamazon;
-INSERT INTO products (product_name, product_description, customer_price, stock_quantity, department)
-VALUES ("NES", "Nintendo Entertainment System", 49.99, 8, 1),
-("SNES", "Super Nintendo Entertainment System", 69.99, 5, 1),
-("NES Controller", "Standard controller for the NES", 8.00, 15, 2),
-("NES Advantage", "1st-party joystick with turbo for NES", 15.00, 2, 2),
-("NES Max", "1st-party alternative to NES controller with turbo", 11.00, 4, 2),
-("Super Mario Bros.", "Original hit platformer featuring Nintendo mascot Mario", 3.00, 23, 3),
-("Mega Man", "Action platformer from Capcom. Defeat Dr. Wily as the heroic Mega Man!", 18.00, 5, 3),
-("Teenage Mutant Ninja Turtles", "Play as the turtles from the hit TV show in this game by Konami", 6.00, 4, 3),
-("Bionic Commando", "A soldier with a bionic arm must rescue Super Joe!", 9.00, 6, 3),
-("SNES Controller", "Standard controller for the SNES", 10, 7, 2),
-("SNES AC Adaptor", "Power supply for SNES", 8, 3, 2),
-("NES RF Modulator", "RF Modulator for NES and SNES", 7, 5, 2),
-("Super Mario World", "Mario's 16-bit premier for SNES. Ride Ypshi!", 9, 6, 4);
+INSERT INTO products (product_name, product_description, customer_price, overhead_cost, stock_quantity, department)
+VALUES ("NES", "Nintendo Entertainment System", 49.99, 200.00, 8, 1),
+("SNES", "Super Nintendo Entertainment System", 69.99, 200.00, 5, 1),
+("NES Controller", "Standard controller for the NES", 8.00, 45.00, 15, 2),
+("NES Advantage", "1st-party joystick with turbo for NES", 15.00, 20.00, 2, 2),
+("NES Max", "1st-party alternative to NES controller with turbo", 11.00, 24.00, 4, 2),
+("Super Mario Bros.", "Original hit platformer featuring Nintendo mascot Mario", 3.00, 23.00, 23, 3),
+("Mega Man", "Action platformer from Capcom. Defeat Dr. Wily as the heroic Mega Man!", 18.00, 75.00, 5, 3),
+("Teenage Mutant Ninja Turtles", "Play as the turtles from the hit TV show in this game by Konami", 6.00, 16.00, 4, 3),
+("Bionic Commando", "A soldier with a bionic arm must rescue Super Joe!", 9.00, 30.00, 6, 3),
+("SNES Controller", "Standard controller for the SNES", 10.00, 56.00, 7, 2),
+("SNES AC Adaptor", "Power supply for SNES", 8.00, 12.00, 3, 2),
+("NES RF Modulator", "RF Modulator for NES and SNES", 7.00, 15.00, 5, 2),
+("Super Mario World", "Mario's 16-bit premier for SNES. Ride Yoshi!", 9.00, 15.00, 6, 4);
 
 USE bamazon;
 SELECT * FROM products;
@@ -62,3 +65,20 @@ WHERE item_id = 13;
 
 USE bamazon;
 SELECT COUNT(*) FROM products;
+
+USE bamazon;
+SELECT department, 
+SUM(stock_quantity)
+FROM products
+GROUP BY department;
+
+USE bamazon;
+SELECT departments.name,
+SUM(products.customer_price)
+FROM departments LEFT JOIN products ON products.department = departments.id
+GROUP BY departments.id;
+
+USE bamazon;
+SELECT departments.id AS ID, departments.name AS Department, SUM(products.overhead_cost) AS Overhead, SUM(products.gross) AS "Gross Sales", SUM(products.gross) - SUM(products.overhead_cost) AS Profits
+FROM departments LEFT JOIN products ON products.department = departments.id
+GROUP BY departments.id;
